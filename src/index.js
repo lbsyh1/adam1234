968b232019c53a5114a576eeaeff14c12e0b3263
adam <1024201a@naver.com>
2022-05-14 15:42:16 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import router1 from '../router/router1';
+import router2 from '../router/router2';
+import router3 from '../router/router3';
+
+
+const app = express();
+
+const PORT =4000;
+
+
+
+
+
+
+
+
+
+
+const handle3=(req,res)=>{
+    console.log("login");
+}
+
+app.use("/",router1);
+app.use("/user",router2);
+app.use("/client",router3);
+
+app.listen(PORT,handle3);
